df9b6d150f126f78c9ee6e9700531f19db743215
Philip Fibranz <philip@fibranz.dev>
2023-12-21 16:05:57 -0500

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,12 +1,8 @@
 import { defineUserConfig } from 'vuepress'
-import { defaultTheme } from '@vuepress/theme-default'
 
 export default defineUserConfig({
   lang: 'en-US',
   title: 'Philip Fibranz',
   // description: 'About Me',
   base: '/',
-  theme: defaultTheme({
-    contributors: false
-  })
 })
