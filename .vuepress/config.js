03fbee0d4963bcccfc54c0104a2f32e671e58d42
Philip Fibranz <philip.fibranz@nexteer.com>
2024-05-11 11:46:47 -0400

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,8 +1,15 @@
 import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from '@vuepress/theme-default'
+import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
   lang: 'en-US',
   title: 'Philip Fibranz',
   // description: 'About Me',
   base: '/',
+  theme: defaultTheme({}),
+  bundler: viteBundler({
+    viteOptions: {},
+    vuePluginOptions: {},
+  }),
 })
